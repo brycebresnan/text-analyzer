9646d24a5279d563ca47a7059375546e8876e242
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-06-26 01:29:50 -0700

    complete the work laid out in the lessons

--- README.md
+++ README.md
@@ -69,6 +69,13 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
+Test: "If an empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
+
 
 Describe: censor()
 
@@ -127,3 +134,34 @@ const text = "The quick zoinks fox jumps over the muppeteer dog"
 const badWords = ["zoinks","muppeteer"]
 censor(badWords, text);
 Expected Output = "The quick **** fox jumps over the **** dog"
+
+
+Describe: boldPassage()
+
+Test: "It should return null if no word or text is entered."
+Code:
+const text = "";
+const word = "";
+numberOfOccurrencesInText(word, text);
+Expected Output: null
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: <p>yo</p>
+
+Test: "It should return a matching word in a strong tag."
+Code:
+const word = "hello";
+const text = "hello";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong></p>
+
+Test: "It should wrap words that match in strong tags but not words that don't."
+Code:
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: <p><strong>hello</strong> there</p>
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+//Utility Logic 
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -15,8 +21,8 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
-    return 0;w
+  if (noInputtedWord(word, text)) {
+    return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
@@ -31,7 +37,6 @@ function numberOfOccurrencesInText(word, text) {
 function censor(badWords, text) {
   let textArray = text.split(" ");
   let indexCounter = 0
-  // let badIndexCounter = 0
   textArray.forEach(function(element) {
     badWords.forEach(function(badElement){
       if (badElement === element) {
@@ -43,3 +48,46 @@ function censor(badWords, text) {
   })
   return textArray.join(" ");
 }
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
+// UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
